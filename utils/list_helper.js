cce173ae870a7a23644e78473ce828a36db452c2
akutuh <akutuh@uef.fi>
2022-07-04 12:52:58 +0300

    apufunktioita ja yksikkötestejä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce(( sum, { likes }) => sum + likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0){
+    return 0
+  } else {
+    const reduced = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+    const { _id, url, __v, ...rest } = reduced
+    console.log(rest)
+    return rest
+  }
+
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
